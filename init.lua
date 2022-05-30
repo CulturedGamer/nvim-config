96ea03f42177f605dea8e2fb3ecb8d1ba338c117
CulturedGamer <105760855+CulturedGamer@users.noreply.github.com>
2022-05-30 21:56:47 +0000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- settings
+require('settings.settings')
+require('settings.keymappings')
+require('settings.colorscheme')
+
+-- lsp
+require('lsp.cmp')
+-- require('lsp.lsp_setup')
+
+-- plugins/plugin configuration
+require('plugins')
+require('treesitter')
+require('statusline')
+require('tabbuffers')
+require('nvimtree')
+require('scope')
+require('dashboard')
+require('autopairs')
