1d24abd01e01c7cb9f0b3b52d00baf1f0620cc7e
Donny D <dony357@outlook.com>
2022-06-04 13:58:04 -0700

    updated statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -17,7 +17,7 @@ options = {
 
    -- section_separators = { left = '', right = '' },
    section_separators = { left = "", right = "" },
-   disabled_filetypes = {'NvimTree', 'packer'},
+   disabled_filetypes = {'NvimTree', 'packer', 'dashboard'},
    always_divide_middle = true,
    globalstatus = false,
  },
