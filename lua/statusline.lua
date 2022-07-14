417bf579fb1c0003e4f80816458457189c7cc2ed
Donny D <dony357@outlook.com>
2022-07-14 13:38:12 -0700

    updated statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -10,36 +10,36 @@
 -- my lualine config:
 
 require('lualine').setup {
-options = {
-   icons_enabled = true,
-   theme = 'auto',
-   component_separators = { left = '', right = ''},
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = '│', right = '' },
 
-   -- section_separators = { left = '', right = '' },
-   section_separators = { left = "", right = "" },
-   disabled_filetypes = {'NvimTree', 'packer', 'dashboard', 'telescope'},
-   always_divide_middle = true,
-   globalstatus = false,
- },
- sections = {
-   lualine_a = {'mode'},
-   lualine_b = {'branch', 'diff', 'diagnostics'},
-   lualine_c = {'filetype'},
-   lualine_x = {},
-   lualine_y = {'encoding'},
-   lualine_z = {'filename'}
- },
- inactive_sections = {
-   lualine_a = {},
-   lualine_b = {},
-   lualine_c = {'filename'},
-   lualine_x = {'location'},
-   lualine_y = {},
-   lualine_z = {}
- },
- tabline = {},
-extensions = {}
-} 
+		-- section_separators = { left = '', right = '' },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { 'NvimTree', 'packer', 'dashboard', 'telescope' },
+		always_divide_middle = true,
+		globalstatus = false,
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filetype' },
+		lualine_x = {},
+		lualine_y = { 'encoding' },
+		lualine_z = { 'filename' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	extensions = {}
+}
 
 -----------------------------------------------------------------------------------------------------------------------
 
